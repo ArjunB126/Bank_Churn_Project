ef94e022dd7ba67e18be9b174cad7b51facaa59f
ArjunB126 <arjunbajare86@gmail.com>
2024-01-13 23:21:38 +0530

    app.py_Changes

--- app (1).py
+++ app (1).py
@@ -10,3 +10,21 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def Home():
     return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        CreditScore = int(request.form['CreditScore'])
+        Age = int(request.form['Age'])
+        Tenure = int(request.form['Tenure'])
+        Balance = float(request.form['Balance'])
+        NumOfProducts = int(request.form['NumOfProducts'])
+        HasCrCard = int(request.form['HasCrCard'])
+        IsActiveMember = int(request.form['IsActiveMember'])
+        EstimatedSalary = float(request.form['EstimatedSalary'])
+        Geography_Germany = request.form['Geography_Germany']
+        if(Geography_Germany == 'Germany'):
+            Geography_Germany = 1
+            Geography_Spain= 0
+            Geography_France = 0
